037bb852357b8979eceb6e80333089ef130ff7b6
subhansumain <zennintoji29@gmail.com>
2025-12-28 20:31:38 +0530

    Update useAuth.js

--- client/src/hooks/useAuth.js
+++ client/src/hooks/useAuth.js
@@ -1,12 +1,12 @@
-import { useContext } from 'react';
-import { AuthContext } from '../context/AuthContext';
+import { useContext } from "react";
+import { AuthContext } from "../auth/AuthContext";
 
 export const useAuth = () => {
   const context = useContext(AuthContext);
-  
+
   if (!context) {
-    throw new Error('useAuth must be used within an AuthProvider');
+    throw new Error("useAuth must be used within an AuthProvider");
   }
-  
+
   return context;
 };
